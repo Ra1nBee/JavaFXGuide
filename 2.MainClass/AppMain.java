603d3ba2fd30571f11c98f456d700963adfebd3e
Ra1nBee <105413553+Ra1nBee@users.noreply.github.com>
2023-01-16 16:41:08 +0900

    Add files via upload

---
+++ 2.MainClass/AppMain.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//AppMain.java
+public class AppMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
